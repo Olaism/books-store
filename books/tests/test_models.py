594a08365ef66047e9aa2b32ccb300d2144ff008
olaism <olaism01@gmail.com>
2023-12-11 08:54:44 -0800

    update tests for books models

--- books/tests/test_models.py
+++ books/tests/test_models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 from django.utils import timezone
 
@@ -8,50 +10,107 @@ from ..models import (
     Review
 )
 
-class AuthorModelTests(TestCase):
+class AuthorModelDefaultTests(TestCase):
 
     def setUp(self):
-        self.author1 = Author(
+        self.author = Author(
             first_name="test",
             last_name="user",
         )
-        self.author2 = Author.objects.create(
+
+    def test_author_listing(self):
+        self.assertTrue(type(self.author.id) == uuid.UUID)
+        self.assertTrue(self.author.id.version, 4)
+        self.assertEqual(self.author.first_name, "test")
+        self.assertEqual(self.author.last_name, "user")
+        self.assertIsNone(self.author.born)
+        self.assertEqual(self.author.get_name, "user test")
+
+    def test_absolute_url(self):
+        self.assertEqual(
+            self.author.get_absolute_url(), 
+            f"/authors/{self.author.id}")
+
+class AuthorModelTests(TestCase):
+
+    def setUp(self):
+        self.author = Author.objects.create(
             first_name="lens",
             last_name="arsenal",
             born=timezone.datetime(1980, 1, 1)
         )
 
-    def test_author1_listing(self):
-        self.assertEqual(self.author1.first_name, "test")
-        self.assertEqual(self.author1.last_name, "user")
-        self.assertEqual(self.author1.born, None)
-
-    def test_author2_listing(self):
-        self.assertEqual(self.author2.first_name, "lens")
-        self.assertEqual(self.author2.last_name, "arsenal")
-        self.assertEqual(self.author2.born, timezone.datetime(1980, 1, 1))
+    def test_author_listing(self):
+        self.assertTrue(type(self.author.id) == uuid.UUID)
+        self.assertTrue(self.author.id.version, 4)
+        self.assertEqual(self.author.first_name, "lens")
+        self.assertEqual(self.author.last_name, "arsenal")
+        self.assertEqual(self.author.born, timezone.datetime(1980, 1, 1))
+        self.assertEqual(self.author.get_name, "arsenal lens")
 
     def test_absolute_url(self):
-        self.assertEqual(self.author2.get_absolute_url(), f"/authors/{self.author2.id}")
+        self.assertEqual(
+            self.author.get_absolute_url(), 
+            f"/authors/{self.author.id}")
 
 
-class BookModelTests(TestCase):
+class BookModelDefaultTests(TestCase):
 
     def setUp(self):
-        self.author = Author.objects.create(first_name="ty", last_name="hy")
+        self.author = Author.objects.create(
+            first_name="Antonio", 
+            last_name="Mele"
+        )
         self.book = Book(
-            title="Hello world",
+            title="Django by example",
             author=self.author,
             price=10.99
         )
 
     def test_book_listing(self):
-        self.assertEqual(self.book.title, "Hello world")
+        self.assertEqual(self.book.title, "Django by example")
         self.assertEqual(self.book.author, self.author)
         self.assertEqual(self.book.price, 10.99)
+        self.assertEqual(self.book.plan_type, "FR")
+        self.assertIsNotNone(self.book.cover)
 
     def test_absolute_url(self):
         self.assertEqual(self.book.get_absolute_url(), f"/books/{self.book.id}")
+
+
+class BookModelTests(TestCase):
+
+    def setUp(self):
+        self.book_cover = SimpleUploadedFile(
+            "sample.png", 
+            b"sample content", 
+            content_type="image/png"
+        )
+        self.sample_doc = SimpleUploadedFile("sample.pdf", b"doc content")
+        self.author = Author.objects.create(
+            first_name="Antonio", 
+            last_name="Mele"
+        )
+        self.book = Book(
+            title="Django by example",
+            author=self.author,
+            price=10.99,
+            cover=self.book_cover,
+            plan_type="PR",
+            doc=self.sample_doc,
+        )
+
+    def test_book_listing(self):
+        self.assertEqual(self.book.title, "Django by example")
+        self.assertEqual(self.book.author, self.author)
+        self.assertEqual(self.book.price, 10.99)
+        self.assertEqual(self.book.cover, self.book_cover)
+        self.assertEqual(self.book.plan_type, "PR")
+        self.assertEqual(self.book.doc, self.sample_doc)
+
+    def test_absolute_url(self):
+        self.assertEqual(self.book.get_absolute_url(), f"/books/{self.book.id}")
+
 
 class ReviewModelTest(BookModelTests):
 
